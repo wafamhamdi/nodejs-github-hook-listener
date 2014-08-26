a61ae6589cc74d822dd46e3a68ad89ac198f175e
William Lepinski <william.lepinski@rga.com>
2014-08-26 15:29:59 -0300

    Fix #1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var app = express();
+var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var exec = require('child_process').exec;
+var crypto = require('crypto');
+var app = express();
 
 if (process.env.GIT_PATH === undefined || process.env.GIT_BRANCH === undefined) {
 	throw new Error('GIT_PATH and GIT_BRANCH should be defined');
@@ -11,14 +12,29 @@ if (process.env.GIT_PATH === undefined || process.env.GIT_BRANCH === undefined) 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-app.post("/", function(req, res) {
-	console.log(req.body, req.headers);
-	var command = exec("cd " + process.env.GIT_PATH + "; git pull --rebase " + process.env.GIT_BRANCH , function (error, stdout, stderr) {
-		console.log(arguments);
-		res.send({ response: true });
-	});
+var verifySignature = function (req, res, next) {
+    var secret = process.env.GIT_SECRET_TOKEN;
+    var hash = crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex');
+    var signature = 'sha1=' + hash;
+
+    if (req.headers['x-hub-signature'] !== signature) {
+        throw new Error('Signature didn\'t match!');
+    }
+
+    next();
+};
+
+var executeUpdate = function (req, res, next) {
+    var command = exec('cd ' + process.env.GIT_PATH + '; git pull --rebase ' + process.env.GIT_BRANCH , function (error, stdout, stderr) {
+        console.log(stdout);
+        next();
+    });
+};
+
+app.post('/', verifySignature, executeUpdate, function (req, res) {
+    res.send({ response: true });
 });
 
 app.listen(3000, function() {
-	console.log("Server listening");
+	console.log('Server listening');
 });
